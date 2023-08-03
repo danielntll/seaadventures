f67929007fac5e1b2314a1ece61e6774b5c2e9ab
danielntll <dev@danielntll.it>
2023-08-03 16:59:48 +0200

    Init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  sassOptions: {
+    includePaths: ["src/styles"],
+    prependData: `
+      @import "./src/styles/variables/colors.scss";
+      @import "./src/styles/variables/fonts.scss";
+      @import "./src/styles/variables/animations.scss";
+      @import "./src/styles/components/badge.scss";
+      @import "./src/styles/components/button.scss";
+      @import "./src/styles/components/container.scss";
+      @import "./src/styles/components/borders.scss";
+      @import "./src/styles/components/gaps.scss";
+      @import "./src/styles/components/paddings.scss";
+      @import "./src/styles/components/margins.scss";
+    `,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
