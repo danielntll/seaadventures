a53a3d8c29bef532c4d6bf2fc8b8bda70d4e9250
danielntll <dev@danielntll.it>
2023-08-03 17:12:18 +0200

    Feat: config styles

--- next.config.js
+++ next.config.js
@@ -7,13 +7,13 @@ const nextConfig = {
       @import "./src/styles/variables/colors.scss";
       @import "./src/styles/variables/fonts.scss";
       @import "./src/styles/variables/animations.scss";
+      @import "./src/styles/variables/borders.scss";
+      @import "./src/styles/variables/gaps.scss";
+      @import "./src/styles/variables/paddings.scss";
+      @import "./src/styles/variables/margins.scss";
       @import "./src/styles/components/badge.scss";
       @import "./src/styles/components/button.scss";
       @import "./src/styles/components/container.scss";
-      @import "./src/styles/components/borders.scss";
-      @import "./src/styles/components/gaps.scss";
-      @import "./src/styles/components/paddings.scss";
-      @import "./src/styles/components/margins.scss";
     `,
   },
 };
